d381a093f49aa8d864b3b6ab0472f021c42294c4
Richard Crippen <richard.crippen@gmail.com>
2021-06-18 14:04:36 -0400

    cleaning up duplicated work - functionality in the public folder already exists for some of the code I was starting to write

--- server.js
+++ server.js
@@ -2,21 +2,51 @@ const express = require('express');
 const colors = require('colors');
 const connectDb = require('./db/connect');
 const morgan = require('morgan');
+const bodyParser = require('body-parser');
 const dotenv = require ('dotenv');
-const workoutRoutes = require ('./routes/workoutRoutes.js');
+const path = require('path');
 
 const port = process.env.PORT || 8080
 
 dotenv.config();
 connectDb();
-const app = express();
 
-app.use (express.json())
-app.use ('/workouts', workoutRoutes)
+const app = express();
+app.use (bodyParser.json())
+app.use(bodyParser.urlencoded({ extended: true}));
+app.use(express.static(path.join(__dirname, '/public')));
+
+
+
+// HTML routing here vs. separate folder
+
+app.get('/', (req, res) => {
+    res.sendFile(path.join(__dirname, 'public', 'index.html'));
+});
+
+app.get ('/stats', (req, res) => {
+    res.sendFile(path.join(__dirname, 'public', 'stats.html'));
+});
+
+app.get ('/exercise', (req, res) => {
+    res.sendFile(path.join(__dirname, 'public', 'exercise.html'));
+});
+
+//API routing below vs. separate file
+
+
+
+
+
+
+
+
 app.get('/', (req, res) => {
     res.send('api is connected')
 })
 
+
+//Connect to PORT and console.log message that server is running for verification
 app.listen (port, () => {
     console.log(`Server running on port ${port}`.yellow.bold)
 })
