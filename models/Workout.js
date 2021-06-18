68ab51741ad17e5e632a62367555eb3c67c49b0e
Richard Crippen <richard.crippen@gmail.com>
2021-06-18 13:30:09 -0400

    needed to add distance to the exerciseSchema

--- models/Workout.js
+++ models/Workout.js
@@ -4,6 +4,7 @@ const exerciseSchema = mongoose.Schema({
     type: {type: String, required: true},
     name: {type: String, required: true},
     duration: {type: Number, required: false},
+    distance: {type: Number, required: false},
     weight: {type: Number, required: false},
     reps: {type: Number, required: false},
     sets: {type: Number, required: false}
