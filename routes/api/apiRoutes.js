80c2375d61c490253b752906ce3a5413dafe954e
Richard Crippen <richard.crippen@gmail.com>
2021-06-23 00:21:52 -0400

    testing API routes still. PUT request is not testing right. Might be me or the code, not sure

--- routes/api/apiRoutes.js
+++ routes/api/apiRoutes.js
@@ -48,15 +48,28 @@ const router = require('express').Router();
                 res.json(err)
             })
     });
+
+//Get workout by ID just for kicks
+
+router.get ('/:id', (req, res) => {
+    db.Workout.find({})
+        .then(data => {
+            res.json(data)
+        })
+        .catch (err => {
+            res.json(err)
+        })
+});
+    
 //Create/post a workout    
-    router.post ('/', async ({ body }, res) => {
+    router.post ('/', ({ body }, res) => {
         db.Workout.create(body)
           .then (dbNewWorkout => { res.json(dbNewWorkout)})
           .catch (err => { res.status(400).json(err)})
     });
 
 //Update a workout based on workout id
-    router.put('/:id', async (req, res) => {
+    router.put('/:id', (req, res) => {
         db.Workout.findByIdAndUpdate(
                 req.params.id,
                 { $push: { 
